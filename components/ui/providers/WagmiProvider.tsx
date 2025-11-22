4c57fe6e483df7859775754d1b44c7f1ecac35b0
soheima canton <soheima.canton@coinbase.com>
2025-11-22 12:56:31 -0300

    added farcaster providers

---
+++ components/ui/providers/WagmiProvider.tsx
@@ -0,0 +1,31 @@
+import { createConfig, http, WagmiProvider } from "wagmi";
+import { base, optimism } from "wagmi/chains";
+import { baseAccount } from "wagmi/connectors";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { farcasterMiniApp } from "@farcaster/miniapp-wagmi-connector";
+import { METADATA } from "@/lib/utils";
+
+export const config = createConfig({
+  chains: [base, optimism],
+  transports: {
+    [base.id]: http(),
+    [optimism.id]: http(),
+  },
+  connectors: [
+    farcasterMiniApp(), 
+    baseAccount({
+      appName: METADATA.name,
+      appLogoUrl: METADATA.iconImageUrl,
+    })
+  ],
+});
+
+const queryClient = new QueryClient();
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
