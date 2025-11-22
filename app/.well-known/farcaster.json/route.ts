b613f1fb4abc0001120a19204703e3d0a3a27730
soheima canton <soheima.canton@coinbase.com>
2025-11-22 14:32:20 -0300

    add manifest

---
+++ app/.well-known/farcaster.json/route.ts
@@ -0,0 +1,34 @@
+import { METADATA } from "../../../lib/utils";
+
+export async function GET() {
+  const config = {
+    accountAssociation: {
+      header:
+        "eyJmaWQiOjEyMTQyLCJ0eXBlIjoiY3VzdG9keSIsImtleSI6IjB4MDRlNkYxMTFlQmY2RkQyNTU3NmQ0ODA0ODA5NjI0MzVEYzNhYThEOCJ9",
+      payload: "eyJkb21haW4iOiJmcmFtZXMtdjItZGVtby1saWxhYy52ZXJjZWwuYXBwIn0",
+      signature:
+        "MHg5MGI1YzA0Zjc3MGY1M2I4M2I3OGQzOTMwNTNjMmJjZjUwNmE3ZThjNDViYmEwNDk2OTcwZTM1ZTQ0YzU2MGU1Nzc4Y2Y1ZTJkNDY2YzE1MWQxNGMzYmFjNzM3ZDcxZGEwZDVjYWJmMGMzZTdhYTc2YzRjMmQ5MmE5NDJhYjkyODFj",
+    },
+      "miniapp": {
+        "version": "1",
+        "name": METADATA.name,
+        "iconUrl": METADATA.iconImageUrl,
+        "homeUrl": METADATA.homeUrl,
+        "imageUrl": METADATA.bannerImageUrl,
+        "splashImageUrl": METADATA.iconImageUrl,
+        "splashBackgroundColor": METADATA.splashBackgroundColor,
+        "description": METADATA.description,
+        "ogTitle": METADATA.name,
+        "ogDescription": METADATA.description,
+        "ogImageUrl": METADATA.bannerImageUrl,
+        "canonicalDomain": "frames-v2-demo-lilac.vercel.app",
+        "noindex": false,
+        "tags": ["base", "baseapp", "miniapp", "demo", "basepay"]
+      },
+      "baseBuilder": {
+        "allowedAddresses": ["0x8342A48694A74044116F330db5050a267b28dD85"],
+      }
+  };
+
+  return Response.json(config);
+}
